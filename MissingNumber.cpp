b56f7a1063fc6cb51365bc5706ef6abf748bacf9
Hemant Suteri <77913443+hemantsuteri@users.noreply.github.com>
2022-09-01 22:16:24 +0530

    Leetcode 268 Missing number problem

---
+++ MissingNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int sum = 0;
+        int length = nums.size();
+        for(int i=0; i < length; i++){
+            sum += nums[i];
+        }
+        int num =((length*(length+1)/2));
+        return(num - sum);
+    }
+};
